83fba75a573429b4b8248f7b2bc54f877b44e6d1
Md. Rayid Hasan Mojumder <39030809+rayid-mojumder@users.noreply.github.com>
2021-08-12 00:02:45 +0600

    Update 3. AVR+LQR.m

--- 3. AVR+LQR.m
+++ 3. AVR+LQR.m
@@ -18,17 +18,41 @@ Gs=Ks/(1+Ts*s);
 Gnum=Ga*Ge*Gg;
 Gdenum=Gs;
 Gavr=Gnum/(1+Gnum*Gdenum);
-%Initiate open loop system without controller
-% convert fractional transfer function to A, B, C, D space matrix
-[num,den] = tfdata(Gavr,'v');
-[A,B,C,D]=tf2ss(num, den)
-[K, P, E]=lqr(A,B,Q,R);
-K
+fraction_n= [0.004 0.454 5.55 15.1 10];%numerator of Gavr, transfer function
+fraction_dn=[1.6e-05 0.002032 0.04732 0.4286 2.133 8.76 18.01 11]; %denominator of Gavr, transfer function
+[A,B,C,D] = tf2ss(fraction_n,fraction_dn) % convert fractional transfer function to A, B, C, D space matrix
+%% Initiate close loop system with LQR controller
+Q=[2 0 0 0 0 0 0;
+    1 2 0 0 0 0 0;
+    0 0 2 0 0 0 0;
+    0 0 0 2 0 0 0;
+    0 0 0 0 2 0 0;
+    0 0 0 0 0 2 0; 
+    0 0 0 0 0 0 2]; 
+%Q=1*eye(7);%positive control weighting matrix, size is similar to A matrix
+% p = 2;
+% Q = p*C'*C
+R=0.00001; %non-negative state weighting matrix
+%% Remember TF to SS is not equal, there is a problem do not directly use tf2ss...%%
+% convert to numerator, denominator first
+% then use tf2ss(num,denom)
+% do not use the following two line, never
+% [num,den] = tfdata(Gavr,'v');
+% [A,B,C,D] = tf2ss(num, den);
+%% Continue %%
+K=lqr(A,B,Q,R); %See how many value is there, could be useful for designing Simulink system
 Af=A-B*K; % Feedback Subtraction node
-sys1=ss(Af,B,C,D);%closed loop system with feedback]
+sys_lqr=ss(Af,B,C,D);%closed loop system with feedback
 t=0:0.01:15;
-Slqr=step(sys1,t);
-plot(t,S,'b',t,Spid,'r',t,Slqr,'g')
-ylabel('Terminal Voltage (pu)')
-xlabel('Time (sec)')
-legend('AVR','AVR PID','AVR LQR')
+Slqr=step(sys_lqr,t);
+% pole(sys_lqr)
+% A
+plot(t,Slqr)
+%sim(AVR_LQR_sim.slx)
+I_lqr= stepinfo(sys_lqr)
+sserror_lqr=abs(1-Slqr(end))
+%rlocus(sys_lqr);
+%bode(sys_lqr)
+% ylabel('Terminal Voltage (pu)')
+% xlabel('Time (sec)')
+% legend('AVR LQR')
